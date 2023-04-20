0a5a2929ff0d8adb54f8d4e6175f43a1a1d5d052
Adgamov Artur <adgamoffartur@yandex.ru>
2023-04-20 16:17:17 +0300

    Добавлен адаптивный header

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Header from "./Haeder";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Haeder.js
@@ -0,0 +1,20 @@
+import React from "react";
+import burger from "../images/burger.svg"
+import logo from "../images/logo.svg"
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="лого" />
+      <div className="header__wrapper">
+        <img className="header__burger" src={burger} alt="бургер"></img>
+        <div className="header__text">
+          <h1 className="header__title">Только самые <span className="header__span">сочные бургеры!</span></h1>
+          <p className="header__subtitle">Бесплатная доставка от 599₽</p>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
